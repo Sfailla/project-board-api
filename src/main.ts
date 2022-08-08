faa08e70bdbea20e10ad1eb6ce17dd38a91abd2f
sfailla <sfailla1983@gmail.com>
2022-08-08 16:19:44 -0400

    refactor postgres init

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,14 @@ import dotenv from 'dotenv'
 import path from 'path'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core/dist/plugin/landingPage/graphqlPlayground'
 import { TerminalColors } from './types/shared'
-import { postgresdb } from './config/postgres-db'
+import { initializePostgresDatabase } from './config/postgres-db'
 
 dotenv.config()
 
 const resolverPaths = path.join(__dirname, '/resolvers/*{.js,.ts}')
-export const entityPaths = path.join(__dirname, '/entities/*{.js,.ts}')
-const terminalStatus = `🚀🎉 [server]: running on http://localhost:${process.env.PORT}/graphql`
+const terminalStatus = `[server]: 🚀🚆 running on http://localhost:${process.env.PORT}/graphql`
 
 const app: Application = express()
 
@@ -32,10 +31,7 @@ const main = async () => {
 	app.use(express.urlencoded({ extended: true }))
 	app.use(cors())
 
-	postgresdb
-		.initialize()
-		.then(() => console.log(TerminalColors.Cyan, 'data source has been initialized!'))
-		.catch(err => console.error(TerminalColors.Red, 'error during data source initialization', err))
+	initializePostgresDatabase()
 
 	await server.start()
 	server.applyMiddleware({ app })

77f1c78f5099afc50b201141e064f95d925f33d6
sfailla <sfailla1983@gmail.com>
2022-08-13 15:43:33 -0400

    add helper fns for generating and verifying jwt

--- src/utils/helper-fns.ts
+++ src/utils/helper-fns.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcryptjs'
+import { sign, verify, JwtPayload, SignOptions, Secret } from 'jsonwebtoken'
+import { JwtCredentials, JwtTokenUser, User } from 'src/types/shared'
 
 export const encryptPassword = async (password: string): Promise<string> => {
 	return await bcrypt.hash(password, Number(process.env.SALT_ROUNDS))
@@ -9,4 +11,22 @@ export const decryptPassword = async (
 	hashedPassword: string
 ): Promise<boolean> => {
 	return await bcrypt.compare(password, hashedPassword)
+}
+
+export const verifyToken = (token: string, secret: Secret): string | JwtPayload => {
+	return verify(token, secret)
+}
+
+export const generateAuthToken = (user: JwtTokenUser): string => {
+	const credentials: JwtCredentials = {
+		iss: process.env.JWT_TOKEN_ISSUER as string,
+		aud: process.env.JWT_TOKEN_AUDIENCE as string,
+		user: {
+			id: user.id,
+			username: user.username,
+			email: user.email
+		}
+	}
+	const exp: SignOptions = { expiresIn: process.env.JWT_TOKEN_EXP as string }
+	return sign(credentials, process.env.JWT_TOKEN_SECRET as string, exp)
 }

58c5df66990957165fed1109324004c1a46640a3
sfailla <sfailla1983@gmail.com>
2022-08-19 16:25:37 -0400

    add logic for logout and check for cookie-token

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -3,11 +3,12 @@ import { verify } from 'jsonwebtoken'
 import { Context, JwtTokenUser } from '../types/shared'
 
 export const isAuthenticated: MiddlewareFn<Context> = ({ context: { req } }, next) => {
-	const token = req.headers.authorization
+	const token = req.headers['x-auth-token'] as string
+	const cookieToken = req.signedCookies['x-auth-token'] as string
 
-	console.log({ tokenHeader: token })
+	console.log({ tokenHeader: token, cookieToken })
 
-	if (!token) {
+	if (!token || !cookieToken || token !== cookieToken) {
 		throw new Error('Not authenticated')
 	}
 

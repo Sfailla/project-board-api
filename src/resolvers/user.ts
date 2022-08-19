d8a8582c8caf22816b248ceb0377651a9770bd0e
sfailla <sfailla1983@gmail.com>
2022-08-19 16:26:59 -0400

    add logout mutation. add UserWithToken and NullUser tgql object-types

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,9 +1,24 @@
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import { Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver, UseMiddleware } from 'type-graphql'
 import { User } from '../entities/user'
 import { Context } from '../types/shared'
 import { postgresdb } from '../config/postgres-db'
 import { decryptPassword, encryptPassword, generateAuthToken, setCookie } from '../utils/helper-fns'
 import { isAuthenticated } from '../middleware/isAuthenticated'
+
+@ObjectType()
+export class NullUser {
+	@Field(() => String, { nullable: true })
+	user: null
+}
+
+@ObjectType()
+export class UserWithToken {
+	@Field(() => User)
+	user: User
+
+	@Field(() => String)
+	token: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -18,7 +33,7 @@ export class UserResolver {
 	@UseMiddleware(isAuthenticated)
 	@Query(() => User, { nullable: true, complexity: 5 })
 	async me(@Ctx() { req }: Context): Promise<User | undefined> {
-		if (!req.user.id) {
+		if (!req.user?.id) {
 			return undefined
 		}
 
@@ -31,13 +46,13 @@ export class UserResolver {
 		return user
 	}
 
-	@Mutation(() => User)
+	@Mutation(() => UserWithToken)
 	async register(
 		@Arg('email', () => String) email: string,
 		@Arg('username', () => String) username: string,
 		@Arg('password', () => String) password: string,
 		@Ctx() { res }: Context
-	): Promise<User> {
+	): Promise<{ user: User; token: string }> {
 		const user = await postgresdb
 			.getRepository(User)
 			.create({
@@ -48,18 +63,19 @@ export class UserResolver {
 			.save()
 
 		const token = generateAuthToken(user)
+
 		res.set('x-auth-token', token)
 		setCookie(res, 'x-auth-token', token)
 
-		return user
+		return { user, token }
 	}
 
-	@Mutation(() => User)
+	@Mutation(() => UserWithToken)
 	async login(
 		@Arg('email', () => String) email: string,
 		@Arg('password', () => String) password: string,
 		@Ctx() { res }: Context
-	): Promise<User> {
+	): Promise<{ user: User; token: string }> {
 		const user = await postgresdb.getRepository(User).findOne({ where: { email } })
 
 		if (!user) {
@@ -73,11 +89,18 @@ export class UserResolver {
 		}
 
 		const token = generateAuthToken(user)
+
 		res.set('x-auth-token', token)
 		setCookie(res, 'x-auth-token', token)
 
-		console.log({ token })
+		return { user, token }
+	}
 
+	@Mutation(() => NullUser)
+	logout(@Ctx() { res, req }: Context): NullUser {
+		const user = { user: null }
+		res.clearCookie('x-auth-token', { maxAge: 0 })
+		req.user = null
 		return user
 	}
 }

e7fc2b9c13af92ace49583c7d0cbe8407f74ef68
Steven-failla_mmcgh <steven.failla@nera.com>
2024-04-09 18:36:55 -0400

    create category entity

---
+++ src/entities/category.ts
@@ -0,0 +1,53 @@
+import { Field, ID, InputType, ObjectType } from 'type-graphql'
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column
+} from 'typeorm'
+import { User } from './user.js'
+
+@Entity()
+@ObjectType()
+export class Category extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Field(() => String)
+  @Column({ unique: true })
+  name: string
+
+  @Field(() => String)
+  @Column()
+  status: string
+
+  @Field(() => User, { nullable: true })
+  @JoinColumn({ name: 'user' })
+  @ManyToOne(() => User, (user) => user.id)
+  user: User | null
+
+  @Field(() => Date)
+  @CreateDateColumn()
+  createdAt: Date
+
+  @Field(() => Date)
+  @UpdateDateColumn()
+  updatedAt: Date
+}
+
+@InputType()
+export class CategoryInput implements Partial<Category> {
+  @Field(() => ID, { nullable: true })
+  id: string
+
+  @Field(() => String, { nullable: true })
+  name: string
+
+  @Field(() => String, { nullable: true })
+  status?: string
+}

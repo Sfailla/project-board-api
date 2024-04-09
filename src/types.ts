5b34a9eb25283f370c1b7cf998c6a0eb023d7da7
Steven-failla_mmcgh <steven.failla@nera.com>
2024-04-09 18:36:45 -0400

    remove unnecessary types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { User } from './entities/user.js'
 
 export enum TerminalColors {
   Magenta = '\x1b[35m%s\x1b[0m',
@@ -13,42 +14,12 @@ export enum TerminalColors {
   Default = '%s'
 }
 
-export enum ProjectBoardStatus {
-  Open = 'open',
-  InProgress = 'in-progress',
-  Review = 'review',
-  Complete = 'complete'
-}
-
-export interface User {
-  id: string
-  firstname?: string | null
-  lastname?: string | null
-  username: string
-  email: string
-  password?: string
-  company?: string | null
-  position?: string | null
-  avatar?: Buffer | string | null
-  createdAt: Date
-}
-
-export type UserId = Pick<User, 'id'>
-
 export type JwtTokenUser = Pick<User, 'id' | 'username' | 'email'>
-
-export interface Tag {
-  id: string
-  name: string
-  color: string
-  createdAt: Date
-  updatedAt: Date
-}
 
 export interface Context {
   req: Request
-  res: Response
-  token: string | string[] | undefined
+  res?: Response
+  token?: string | string[] | undefined
 }
 
 export interface JwtCredentials {

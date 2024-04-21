8d049ceeea901ed53ea85963a986fc36e2ef465d
Steven-failla_mmcgh <steven.failla@nera.com>
2024-04-21 15:00:55 -0400

    add type casting

--- src/utils/helper-fns.ts
+++ src/utils/helper-fns.ts
@@ -23,18 +23,20 @@ export const verifyToken = (
 
 export const generateAuthToken = (user: JwtTokenUser): string => {
   const credentials: JwtCredentials = {
-    iss: process.env.JWT_TOKEN_ISSUER as string,
-    aud: process.env.JWT_TOKEN_AUDIENCE as string,
+    iss: <string>process.env.JWT_TOKEN_ISSUER,
+    aud: <string>process.env.JWT_TOKEN_AUDIENCE,
     user: {
       id: user.id,
       username: user.username,
       email: user.email
     }
   }
+
   const exp: jwt.SignOptions = {
-    expiresIn: process.env.JWT_TOKEN_EXPIRATION as string
+    expiresIn: <string>process.env.JWT_TOKEN_EXPIRATION
   }
-  return jwt.sign(credentials, process.env.JWT_TOKEN_SECRET as string, exp)
+
+  return jwt.sign(credentials, <string>process.env.JWT_TOKEN_SECRET, exp)
 }
 
 export const setCookie = (
@@ -51,8 +53,6 @@ export const setCookie = (
     maxAge: 1000 * 60 * 60 * 24 * 7,
     domain: 'localhost'
   }
-
-  console.log({ cookieOptions, cookieToken: cookieValue })
 
   res.cookie(cookieName, cookieValue, { ...cookieOptions, ...options })
 }
